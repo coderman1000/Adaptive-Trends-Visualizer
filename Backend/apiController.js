4b939d4cc4d339eb8e37b043550e0c2b98c74221
ahsan1000 <ahsan.codeman@gmail.com>
2024-07-25 09:01:00 +0500

    Backend node/express code

---
+++ Backend/apiController.js
@@ -0,0 +1,77 @@
+const mongoose = require("mongoose");
+const common = require("./common");
+
+exports.getTableAndColumnNames = async (req, res) => {
+  try {
+    const dbName = req.params.dbName;
+    const db = mongoose.connection.useDb(dbName);
+    const collections = await db.db.listCollections().toArray();
+
+    const result = await Promise.all(
+      collections.map(async (collection) => {
+        const collectionInfo = await db.collection(collection.name).findOne();
+        if (collectionInfo) {
+          const columns = Object.keys(collectionInfo).filter(
+            (column) => column !== "_id" && column !== "InsertedDateTime"
+          );
+          return {
+            tableName: collection.name,
+            columns: columns,
+          };
+        } else {
+          return {
+            tableName: collection.name,
+            columns: [],
+          };
+        }
+      })
+    );
+
+    res.json(result);
+  } catch (error) {
+    common.handleError(res, error);
+  }
+};
+
+exports.getColumnValuesByTimeInterval = async (req, res) => {
+  try {
+    const { dbName, collectionName, columns, startTime, endTime } = req.body;
+    const db = mongoose.connection.useDb(dbName);
+
+    // Convert startTime and endTime to ISO string format
+    const startTimeStr = new Date(startTime).toISOString();
+    const endTimeStr = new Date(endTime).toISOString();
+
+    const query = {
+      InsertedDateTime: {
+        $gte: startTimeStr,
+        $lte: endTimeStr,
+      },
+    };
+    const projection = columns.reduce(
+      (proj, col) => ({ ...proj, [col]: 1 }),
+      {}
+    );
+    // Include InsertedDateTime and exclude _id from the projection
+    projection.InsertedDateTime = 1;
+    projection._id = 0;
+
+    console.log("Query:", JSON.stringify(query));
+    console.log("Projection:", JSON.stringify(projection));
+
+    // Find documents without applying projection to debug
+    const rawResult = await db.collection(collectionName).find(query).toArray();
+    console.log("Raw Result:", rawResult);
+
+    const result = await db
+      .collection(collectionName)
+      .find(query)
+      .project(projection)
+      .toArray();
+    console.log("Result:", result);
+
+    res.json(result);
+  } catch (error) {
+    common.handleError(res, error);
+  }
+};
